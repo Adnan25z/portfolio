e81915f78c399f24a3333222c26db2bf754ebdc4
Adnan <adnanzafar2001@gmail.com>
2023-11-04 17:10:18 +1100

    added react icons and links added to footer

--- src/screens/footer/Footer.jsx
+++ src/screens/footer/Footer.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Footer.css';
-
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { MdOutlineAttachEmail } from "react-icons/md";
 const Footer = () => {
     return (
         <footer className="footer" id="contact">
             <div className="footer-content">
-                <h3>Your Name</h3>
+                <h3>Adnan Zafar</h3>
                 <ul className="footer-links">
-                    <li><a href="#link1">Link 1</a></li>
-                    <li><a href="#link2">Link 2</a></li>
-                    <li><a href="#link3">Link 3</a></li>
+                    <li><a href="https://www.linkedin.com/in/adnan2510" target="_blank" rel="noopener noreferrer" title="Connect with me on LinkedIn">LinkedIn <BsLinkedin/></a></li>
+                    <li><a href="https://github.com/Adnan25z" target="_blank" rel="noopener noreferrer" title="Check my work on GitHub">GitHub <BsGithub/></a></li>
+                    <li><a href="mailto:adnanzafar2001@gmail.com" target="_blank" rel="noopener noreferrer" title="Contact me via e-mail">E-mail <MdOutlineAttachEmail/></a></li>
                 </ul>
             </div>
         </footer>
